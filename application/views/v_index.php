8affe75499fbb5b81c03f1ab3361f7aba74288e3
Mfauzanrahmat19 <mfauzanrahmat19@gmail.com>
2021-10-19 17:19:42 +0700

    Membuat form validation dan membuat sistem crud

---
+++ application/views/v_index.php
@@ -0,0 +1,12 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus animi fugiat temporibus assumenda amet perferendis repellat! Accusamus earum ea temporibus quo dolore expedita omnis placeat. Voluptatibus maxime dolorum dignissimos quibusdam!
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis dolorem earum eum. Officia aut veritatis inventore totam distinctio incidunt, ullam quasi laboriosam? Nesciunt perferendis officiis, quis vel magni iusto hic.
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia nostrum ab reiciendis nisi laborum cumque inventore ullam autem quibusdam aspernatur atque ut eos iste voluptatibus, hic ipsum repellendus ratione est.
+</p>
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem id sapiente odit vitae distinctio voluptates ipsum libero in dicta laborum! Vitae debitis ex repudiandae culpa iure nulla fuga iusto nam?
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi maiores voluptatibus, harum similique officia pariatur distinctio, facere odio architecto, veniam provident praesentium eaque illum error ab debitis qui quod suscipit.
+</p>
+</section>
